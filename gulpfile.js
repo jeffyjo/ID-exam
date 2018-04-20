e8da821f689ef2fe5d78f7266dcabedbb8dbea0d
jeffyjo <jeff_1992_1992@hotmail.com>
2018-04-20 10:20:41 +0200

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 let gulp    = require('gulp')
     bs      = require('browser-sync').create()
 
+let folder = {
+    src: 'src/'
+}
+
 gulp.task( 'serve', () => {
     bs.init({
         server: './src/',
         browser: 'google chrome',
         port: 5000
     })
-
-    gulp.watch('src/**/*.html').on( 'change', bs.reload )
-    gulp.watch('src/css/**/*.css').on( 'change', bs.reload )
-    gulp.watch('src/js/**/*.js').on( 'change', bs.reload )
 })
 
-gulp.task( 'default', ['serve'] )
+gulp.task( 'watch', ['serve'], () => {
+    gulp.watch(folder.src + '**/*.html').on( 'change', bs.reload )
+    gulp.watch(folder.src + 'css/**/*.css').on( 'change', bs.reload )
+    gulp.watch(folder.src + 'js/**/*.js').on( 'change', bs.reload )
+})
+
+gulp.task( 'default', ['watch'] )
