f4108cc1608dbcd55de891d2e185bdbaff6803be
jkhoejgaard <jkh@stromworks.com>
2018-04-22 22:14:03 +0200

    Add gulp task for sass & add build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,42 @@
-let gulp    = require('gulp')
-    bs      = require('browser-sync').create()
+let gulp          = require('gulp')
+    sass          = require('gulp-sass')
+    sourcemaps    = require('gulp-sourcemaps')
+    autoprefixer  = require('gulp-autoprefixer')
+    bs            = require('browser-sync').create()
 
 let folder = {
-    src: 'src/'
+    src: 'src/',
+    build: 'build/'
 }
+
+let path = {
+    html: {
+        src: '**/*.html'
+    },
+    sass: {
+        src: 'sass/**/*.scss',
+        build: 'css',
+        dist: 'css'
+    },
+    js: {
+        src: 'js/**/*.js',
+        build: 'js',
+        dist: 'js'
+    }
+}
+
+let autoprefixerOptions = {
+    browsers: ['last 2 versions']
+}
+
+gulp.task( 'sass', () => {
+    return gulp.src(folder.src + path.sass.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(folder.build + path.sass.build));
+})
 
 gulp.task( 'serve', () => {
     bs.init({
@@ -13,10 +46,10 @@ gulp.task( 'serve', () => {
     })
 })
 
-gulp.task( 'watch', ['serve'], () => {
-    gulp.watch(folder.src + '**/*.html').on( 'change', bs.reload )
-    gulp.watch(folder.src + 'css/**/*.css').on( 'change', bs.reload )
-    gulp.watch(folder.src + 'js/**/*.js').on( 'change', bs.reload )
+gulp.task( 'watch', ['sass', 'serve'], () => {
+    gulp.watch(folder.src + path.html.src).on( 'change', bs.reload )
+    gulp.watch(folder.src + path.sass.src).on( 'change', bs.reload )
+    gulp.watch(folder.src + path.js.src).on( 'change', bs.reload )
 })
 
 gulp.task( 'default', ['watch'] )
